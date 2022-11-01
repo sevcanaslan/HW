911e153f7d50a0340c740e9ee7dbe1eb0b439421
sevcanaslan <116001272+sevcanaslan@users.noreply.github.com>
2022-11-01 17:06:28 +0300

    Ödev2

--- src/Soru1.java
+++ src/Soru1.java
@@ -3,36 +3,24 @@ import java.util.Scanner;
 public class Soru1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//kullanıcıdan bir kelime alın
-				// eğer kelime a ile başlıyorsa kelimenin son indexini ekrana bastırın
-
-				// eğer kelime b ile başlıyorsa ilk indexi "@" ile değiştirin
-
-				//eğer a veya b ile başlamıyorsa kelimenin birinci indexini bastırın
-
-				//replace metohud
-				//charAt()
-				//kelime.startsWith("a")
-				//length()
-
-						Scanner scanner = new Scanner(System.in);
-						System.out.println("Bir kelime giriniz: ");
-						String kelime = scanner.next();
-						int a= kelime.charAt(0);
-						if(kelime.startsWith("a")) {
-							System.out.println("son index: " + kelime.charAt(kelime.length()-1));
-						}
-						else if(kelime.startsWith("b")) {
-							System.out.println("yeni kelime: " + kelime.replaceFirst("b", "@"));
-						}
-						
-						else{
-							System.out.println("1. karekter: " + kelime.charAt(0));
-						
-					}
-				
+		
+	    //bir dizide 13 var ise 13 ve bir sonraki eleman
+        //toplama eklenmeden toplam sonucu bulan kod
+        //çarşamba
+        
+        int toplam =0;
+        int nums[] = {13, 1, 13, 13, 13, 2, 5, 13, 1,20,13,2,5};
+        for (int i = 0; i <nums.length; i++) {
+        if (nums[i] == 13 || (i > 0 && nums[i - 1] == 13)) {    
+        continue;      
+        } else {
+       toplam = toplam + nums[i];
+        		    }
+        
+	}
+        System.out.println(toplam);
 
 	}
+
 
 }
